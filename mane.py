52ad1b28b02614ed8f93c8cb62760095340d4e3f
Rostyslav Zinchenko <rostik.zin4enko@gmail.com>
2022-11-08 00:25:41 +0200

    Commit 4. modi mane.py/add new function

--- mane.py
+++ mane.py
@@ -4,3 +4,11 @@ def mane_function(n):
     return n + mane_function(n - 1)
 
 print(mane_function(10))
+
+def linear_search(lys, element):
+    for i in range (len(lys)):
+        if lys[i] == element:
+            return i
+    return -1
+
+print(linear_search([1, 2, 3, 4, 5, 2, 1], 2))
