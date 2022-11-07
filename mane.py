e8d29ef1d852845df1dbb03546588653ba85b312
Rostyslav Zinchenko <rostik.zin4enko@gmail.com>
2022-11-08 00:30:49 +0200

    Commit test 1. Add test_function()

--- mane.py
+++ mane.py
@@ -4,3 +4,17 @@ def mane_function(n):
     return n + mane_function(n - 1)
 
 print(mane_function(10))
+
+def test_function(lys, val):
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if lys[mid] == val:
+            index = mid
+        else:
+            if val<lys[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    return index
+
+print(test_function([10,20,30,40,50,60,70,80,90,100], 20))
