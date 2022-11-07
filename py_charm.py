4ea95a13de31ea0bb412d0c376e05c5cc3b09346
Rostyslav Zinchenko <rostik.zin4enko@gmail.com>
2022-11-08 00:40:12 +0200

    Commit main 7. Modi py_charm.py

---
+++ py_charm.py
@@ -0,0 +1,3 @@
+dict_1 = {name: 'Rostyslav', surname: 'Zinchenko', age: 21}
+dict_2 = {breakfast: 'milk', lunch: 'soup', dinner: 'buckwheat'}
+dict_3 = {The_Green_book: 8.5, The_Lighthouse: 7.6, Se7en: 8.6}
