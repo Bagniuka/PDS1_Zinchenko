82edcdc129abaf8fe71ee4905125caf93559a748
Rostyslav Zinchenko <rostik.zin4enko@gmail.com>
2022-11-08 00:44:24 +0200

    Commit main 8. Modi py_charm.py

--- py_charm.py
+++ py_charm.py
@@ -1,3 +1,12 @@
 dict_1 = {name: 'Rostyslav', surname: 'Zinchenko', age: 21}
 dict_2 = {breakfast: 'milk', lunch: 'soup', dinner: 'buckwheat'}
 dict_3 = {The_Green_book: 8.5, The_Lighthouse: 7.6, Se7en: 8.6}
+
+def new_file(*dict, name):
+    with open(name, 'w', encoding='utf8') as f:
+        for i in dict:
+            for k, v in i.items():
+                kv = str(k) + ': ' + str(v) + '\n'
+                f.write(kv)
+
+new_file(dict_1, dict_2, dict_3, name="DZ_5_New_file")
