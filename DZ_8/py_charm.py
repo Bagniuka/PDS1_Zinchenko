0fb26bf2724bbf5d343730c240fc5797850cef20
Rostyslav Zinchenko <rostik.zin4enko@gmail.com>
2022-11-08 17:31:07 +0200

    Little changes

--- DZ_8/py_charm.py
+++ DZ_8/py_charm.py
@@ -1,6 +1,7 @@
-dict_1 = {name: 'Rostyslav', surname: 'Zinchenko', age: 21}
-dict_2 = {breakfast: 'milk', lunch: 'soup', dinner: 'buckwheat'}
-dict_3 = {The_Green_book: 8.5, The_Lighthouse: 7.6, Se7en: 8.6}
+dict_1 = {'name': 'Rostyslav', 'surname': 'Zinchenko', 'age': 21}
+dict_2 = {'breakfast': 'milk', 'lunch': 'soup', 'dinner': 'buckwheat'}
+dict_3 = {'The_Green_book': 8.5, 'The_Lighthouse': 7.6, 'Se7en': 8.6}
+
 
 def new_file(*dict, name):
     with open(name, 'w', encoding='utf8') as f:
@@ -9,10 +10,12 @@ def new_file(*dict, name):
                 kv = str(k) + ': ' + str(v) + '\n'
                 f.write(kv)
 
+
 new_file(dict_1, dict_2, dict_3, name="DZ_5_New_file")
 
 import os
 import os.path
+
 
 def filedir_this_dir(path):
     for i in os.listdir(path):
@@ -22,7 +25,9 @@ def filedir_this_dir(path):
             print(i)
     print(f'Загальна кількість файлів та каталогів в поточній директиві: {len(os.listdir(path))}')
 
+
 filedir_this_dir("D:\\Start_Up\\DZ_5")
+
 
 def new_dict(name):
     new_dict = {}
@@ -32,5 +37,6 @@ def new_dict(name):
             key_1 = key.replace(':', '')
             new_dict[key_1] = value
     return new_dict
+
 
 print(new_dict('DZ_5_New_file'))
