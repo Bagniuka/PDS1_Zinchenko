d762ac77bb6a4c4e4bc293a09fb8854e536f70b4
Rostyslav Zinchenko <rostik.zin4enko@gmail.com>
2022-12-08 21:30:10 +0200

    DZ_14 (Add func: delete_node, max_in_min, find_min, find_max, insert_list)

--- DZ_14/task_1.py
+++ DZ_14/task_1.py
@@ -9,22 +9,38 @@ class Tree:
         return str(self.id_node)
 
     # Insert method to create nodes
-    def insert(self, id_node):
+    def insert(self, node):
         if self.id_node:
-            if id_node < self.id_node:
+            if node < self.id_node:
                 if self.left is None:
-                    self.left = Tree(id_node)
+                    self.left = Tree(node)
                 else:
-                    self.left.insert(id_node)
-            elif id_node > self.id_node:
+                    self.left.insert(node)
+            elif node > self.id_node:
                 if self.right is None:
-                    self.right = Tree(id_node)
+                    self.right = Tree(node)
                 else:
-                    self.right.insert(id_node)
+                    self.right.insert(node)
         else:
-            self.id_node = id_node
+            self.id_node = node
 
-    # findval method to compare the id_node with nodes
+    def insert_list(self, list):
+        for node in list:
+            if self.id_node:
+                if node < self.id_node:
+                    if self.left is None:
+                        self.left = Tree(node)
+                    else:
+                        self.left.insert(node)
+                elif node > self.id_node:
+                    if self.right is None:
+                        self.right = Tree(node)
+                    else:
+                        self.right.insert(node)
+            else:
+                self.id_node = node
+
+    # Findval method to compare the id_node with nodes
     def findval(self, find_val):
         if find_val < self.id_node:
             if self.left is None:
@@ -44,3 +60,51 @@ class Tree:
         print(self.id_node),
         if self.right:
             self.right.print_tree()
+
+    # Find max value
+    def find_max(self):
+        if self.right:
+            self.right.find_max()
+        else:
+            print(f'The max values is {self.id_node}')
+
+    # Find min value
+    def find_min(self):
+        if self.left:
+            self.left.find_min()
+        else:
+            print(f'The min value is {self.id_node}')
+
+    # Function to find max value in the branch of minimum values to replace deleted node in case it has 2 children nodes
+    def __max_in_min(self, iter_amm):
+        if self.left and iter_amm == 0:
+            self.left.__max_in_min(iter_amm=1)
+        elif self.right and iter_amm == 1:
+            self.right.__max_in_min(iter_amm=1)
+        else:
+            return self.id_node
+
+    # Deleting node out of binary search tree (four possible variants)
+    def delete_node(self, node):
+        if node == self.id_node:
+            if not self.right and not self.right:
+                self.id_node = None
+            if self.left and not self.right:
+                self.id_node = self.left
+            if self.right and not self.left:
+                self.id_node = self.right
+            if self.right and self.left:
+                self.id_node = self.__max_in_min(0)
+        else:
+            if node > self.id_node:
+                self.right.delete_node(node)
+            if node < self.id_node:
+                self.left.delete_node(node)
+
+
+t1 = Tree(5)
+my_list = [2, 1, 0, 3, 11, 9, 8, 10, 14, 12, 6, 13, 17]
+t1.insert_list(my_list)
+t1.print_tree()
+t1.delete_node(2)
+t1.print_tree()
