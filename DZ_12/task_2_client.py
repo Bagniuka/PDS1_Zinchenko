5dcfbd3077ea428784067138b6ca88da3146997b
Rostyslav Zinchenko <rostik.zin4enko@gmail.com>
2022-12-20 00:07:44 +0200

    DZ_12

--- DZ_12/task_2_client.py
+++ DZ_12/task_2_client.py
@@ -3,7 +3,7 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
     c.connect(('localhost', 55555))
     while True:
-        message = input('Enter number: ')
+        message = input('Enter numbers: ')
         c.sendall(message.encode())
         message = c.recv(1024)
         print(message.decode())
